baa52d2cd5855e2a6356d2fca176c19cefa9b083
ardatdat <ardatdat@gmail.com>
2011-01-06 11:49:41 +0800

    Clock frequency for 250/300/1000/1152/1200 Mhz

--- arch/arm/plat-omap/include/mach/omap34xx.h
+++ arch/arm/plat-omap/include/mach/omap34xx.h
@@ -132,8 +132,12 @@
 #define VDD1_THRESHOLD	VDD1_OPP3
 
 /* MPU speeds */
-#define S1100M  1100000000
+/* Non-standard MPU speeds */
+#define S1100M  1152000000
 #define S1200M  1200000000
+#define S1300M  1300000000
+
+/* Standard MPU speeds */
 #define S1000M  1000000000
 #define S800M   800000000
 #define S720M   720000000
