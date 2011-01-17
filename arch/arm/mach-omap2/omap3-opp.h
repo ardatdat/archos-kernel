ef7ee511cd686b8632b867877bc6073e991be47c
ardatdat <ardatdat@gmail.com>
2011-01-18 01:57:51 +0800

    freq: Stable frequency at 300,600,1000,1100,1280 Mhz

--- arch/arm/mach-omap2/omap3-opp.h
+++ arch/arm/mach-omap2/omap3-opp.h
@@ -9,16 +9,16 @@ static struct omap_opp omap3630_mpu_rate_table[] = {
 	{S600M,  VDD1_OPP2, 0x2d, 0x0, 0x0},
 	{S1000M, VDD1_OPP3, 0x3c, 0x0, 0x0},
 	{S1100M, VDD1_OPP4, 0x3d, 0x0, 0x0},
-	{S1200M, VDD1_OPP5, 0x3e, 0x0, 0x0},
+	{S1280M, VDD1_OPP5, 0x40, 0x0, 0x0},
 };
 
 static struct omap_opp omap3630_dsp_rate_table[] = {
 	{0, 0, 0},
 	{S260M, VDD1_OPP1, 0x1f, 0x0, 0x0},
 	{S520M, VDD1_OPP2, 0x2d, 0x0, 0x0},
-	{S800M, VDD1_OPP3, 0x3c, 0x0, 0x0},
+	{S400M, VDD1_OPP3, 0x3c, 0x0, 0x0},
 	{S400M, VDD1_OPP4, 0x3d, 0x0, 0x0},
-	{S400M, VDD1_OPP5, 0x3e, 0x0, 0x0},
+	{S100M, VDD1_OPP5, 0x40, 0x0, 0x0},
 };
 
 static struct omap_opp omap3630_l3_rate_table[] = {
@@ -37,77 +37,5 @@ static struct omap_opp omap3611_l3_rate_table[] = {
 	{S166M, VDD2_OPP2, 0x2d, 0x0, 0x0},
 };
 
-static struct omap_opp omap3_mpu_rate_table[] = {
-	{0, 0, 0},
-	/*OPP1*/
-	{S125M, VDD1_OPP1, 0x1E},
-	/*OPP2*/
-	{S250M, VDD1_OPP2, 0x26},
-	/*OPP3*/
-	{S500M, VDD1_OPP3, 0x30},
-	/*OPP4*/
-	{S550M, VDD1_OPP4, 0x36},
-	/*OPP5*/
-	{S600M, VDD1_OPP5, 0x3C},
-	/*OPP6*/
-	{S720M, VDD1_OPP6, 0x3C},
-};
-
-static struct omap_opp omap3_l3_rate_table[] = {
-	{0, 0, 0},
-	/*OPP1*/
-	{0, VDD2_OPP1, 0x1E},
-	/*OPP2*/
-	{S83M, VDD2_OPP2, 0x24},
-	/*OPP3*/
-	{S166M, VDD2_OPP3, 0x2C},
-};
-
-/* iva rate table for 3420 */
-static struct omap_opp omap3_dsp_rate_table_3420[] = {
-	{0, 0, 0},
-	/*OPP1*/
-	{S90M, VDD1_OPP1, 0x1E},
-	/*OPP2*/
-	{S180M, VDD1_OPP2, 0x26},
-	/*OPP3*/
-	{S360M, VDD1_OPP3, 0x30},
-	/*OPP4*/
-	{S360M, VDD1_OPP4, 0x36},
-	/*OPP5*/
-	{S360M, VDD1_OPP5, 0x36},
-};
-
-/* iva rate table for 3430 */
-static struct omap_opp omap3_dsp_rate_table[] = {
-	{0, 0, 0},
-	/*OPP1*/
-	{S90M, VDD1_OPP1, 0x1E},
-	/*OPP2*/
-	{S180M, VDD1_OPP2, 0x26},
-	/*OPP3*/
-	{S360M, VDD1_OPP3, 0x30},
-	/*OPP4*/
-	{S430M, VDD1_OPP4, 0x36},
-	/*OPP5*/
-	{S430M, VDD1_OPP5, 0x3C},
-};
-
-/* iva rate table for 3430 */
-static struct omap_opp omap3_dsp_rate_table_3440[] = {
-	{0, 0, 0},
-	/*OPP1*/
-	{S90M, VDD1_OPP1, 0x1E},
-	/*OPP2*/
-	{S180M, VDD1_OPP2, 0x26},
-	/*OPP3*/
-	{S360M, VDD1_OPP3, 0x30},
-	/*OPP4*/
-	{S430M, VDD1_OPP4, 0x36},
-	/*OPP5*/
-	{S430M, VDD1_OPP5, 0x3C},
-	/*OPP6*/
-	{S520M, VDD1_OPP6, 0x3C},
-};
 
 #endif
