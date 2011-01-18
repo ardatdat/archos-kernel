8858fc49987077830e69f5e4a790c49d748efe2f
ardatdat <ardatdat@gmail.com>
2011-01-18 20:52:07 +0800

    freq: Stable frequency at 300,600,1000,1100,1200 Mhz

--- arch/arm/mach-omap2/omap3-opp.h
+++ arch/arm/mach-omap2/omap3-opp.h
@@ -5,8 +5,8 @@
 
 static struct omap_opp omap3630_mpu_rate_table[] = {
 	{0, 0, 0},
-	{S250M, VDD1_OPP1, 0x1F, 0x0, 0x0},
-	{S300M, VDD1_OPP2, 0x20, 0x0, 0x0},
+	{S300M,  VDD1_OPP1, 0x20, 0x0, 0x0},
+	{S600M,  VDD1_OPP2, 0x2d, 0x0, 0x0},
 	{S1000M, VDD1_OPP3, 0x3c, 0x0, 0x0},
 	{S1100M, VDD1_OPP4, 0x3d, 0x0, 0x0},
 	{S1200M, VDD1_OPP5, 0x3d, 0x0, 0x0},
@@ -35,11 +35,11 @@ static struct omap_opp omap3630_dsp_rate_table[] = {
 	/*OPP2 (OPP100) - 1.1625V*/
 	{S520M, VDD1_OPP2, 0x2d, 0x0, 0x0},
 	/*OPP3 (OPP130) - 1.3V*/
-	{S660M, VDD1_OPP3, 0x38, 0x0, 0x0},
+	{S400M, VDD1_OPP3, 0x3c, 0x0, 0x0},
 	/*OPP4 (OPP-1G) - 1.35V*/
-	{S800M, VDD1_OPP4, 0x3c, 0x0, 0x0},
+	{S90M,  VDD1_OPP4, 0x3d, 0x0, 0x0},
 	/*OPP5 (OPP-1.3G) - 1.35V*/
-	{S400M, VDD1_OPP5, 0x3c, 0x0, 0x0},
+	{S90M,  VDD1_OPP5, 0x3d, 0x0, 0x0},
 };
 
 static struct omap_opp omap3_mpu_rate_table[] = {
