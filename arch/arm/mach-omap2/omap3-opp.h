6e8cccc85942d758f51f2852ba7594a79a20f788
ardatdat <ardatdat@gmail.com>
2011-01-29 23:05:33 +0800

    cflags: CFLAGS_KERNEL optimization dsp: Set 90Mhz for high mpu rate config: enable EXT4

--- arch/arm/mach-omap2/omap3-opp.h
+++ arch/arm/mach-omap2/omap3-opp.h
@@ -31,7 +31,7 @@ static struct omap_opp omap3630_dsp_rate_table[] = {
 	/*OPP4 (OPP-1G) - 1.35V*/
 	{S800M, VDD1_OPP4, 0x3c, 0x0, 0x0},
 	/*OPP5 (OPP-1.3G) - 1.35V*/
-	{S65M,  VDD1_OPP5, 0x41, 0x0, 0x0},
+	{S90M,  VDD1_OPP5, 0x41, 0x0, 0x0},
 };
 
 static struct omap_opp omap3611_l3_rate_table[] = {
